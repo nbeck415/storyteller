27aea2412c4f64d8daad0a37da97e70520b77cdf
nbeck415 <natalielunbeck@yahoo.com>
2020-06-06 11:43:12 -0700

    Create gpt2model.py

---
+++ gpt2model.py
@@ -0,0 +1,6 @@
+#Natalie Lunbeck
+#This program creates a GPT2 Model and uses it to generate text
+
+import torch
+import numpy as np
+from transformers import GPT2Model, GPT2Config, GPT2Tokenizer, GPT2TokenizerFast
