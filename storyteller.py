24ebed74ba55cbb32f0ff56ac18b768fa492608c
nbeck415 <natalielunbeck@yahoo.com>
2020-06-06 12:00:45 -0700

    Update storyteller.py

--- storyteller.py
+++ storyteller.py
@@ -7,5 +7,18 @@ from transformers import GPT2Model, GPT2Config, GPT2Tokenizer, GPT2TokenizerFast
 
 class Storyteller:
 
-    def __init__(self, source, num_words, prompt):
-        
+    def __init__(self, source, num_words, prompt="DEFAULT"):
+        #where the training data is stored
+        self.source = source
+        #length of output
+        self.num_words = num_words
+        #topic (?)
+        self.prompt = prompt
+        #make the GPT2 model with given configuration
+        self.model_config = GPT2Model(self.makeConfig)
+
+    def makeConfig():
+        #configuration object with modifiable parameters
+        vocab_size = 10000
+        config = GPT2Config(vocab_size=vocab_size)
+        return config
