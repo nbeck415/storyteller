e93e21e425038291a1cec17fc7e84b16c2c4d584
nbeck415 <natalielunbeck@yahoo.com>
2020-06-06 14:38:50 -0700

    increased speed and efficiency

--- storyteller_simple.py
+++ storyteller_simple.py
@@ -2,7 +2,7 @@
 #This program creates a simple GPT2 Model and uses it to generate text
 
 import torch
-import numpy as np
+#import numpy as np
 import gpt_2_simple as gpt2
 
 class StorytellerSimple:
@@ -15,30 +15,35 @@ class StorytellerSimple:
         #topic (?)
         self.prompt = prompt
         #first-time runthrough
-        setupModel()
+        #self.setupModel()
+        #print('setup complete')
         self.session = gpt2.start_tf_sess()
         #story generator, give parameters if necessary
-        runGenerator()
-        loadRun()
+        self.runGenerator()
+        print('training complete')
+        self.loadRun()
+        print('done')
 
 
-    def setupModel(size="124M"):
+    def setupModel(self, size="124M"):
         #create a gpt2 model based on dataset
         #first time runthrough, do not run twice
         gpt2.download_gpt2(model_name=size)
 
-    def runGenerator(num_words=self.num_words, variance=0.9):
-        session = self.session
-        gpt2.load_gpt2(session)
-        gpt2.finetune(session, dataset=self.source, model_name='124M')
+    def runGenerator(self, num_words=200, variance=0.9):
+        session = self.session #gpt2.start_tf_sess()
+        print('here')
+        #gpt2.load_gpt2(session)
+        print('there')
+        gpt2.finetune(session, dataset=self.source, model_name='124M', steps=50, restore_from='fresh',run_name='run1',sample_every=200,save_every=500, print_every=10)
         sentence = self.prompt
         default_sentence = "The quick brown fox jumped over the lazy dog"
         if sentence == "DEFAULT":
             sentence = default_sentence
-        gpt2.generate(session, length=num_words, temperature=variance, top_k=1, top_p=0.9,
+        result = gpt2.generate(session, length=num_words, temperature=variance, top_k=1, top_p=0.9,
               run_name='run1', prefix=sentence, return_as_list=True)
 
-    def loadRun():
+    def loadRun(self):
         gpt2.load_gpt2(self.session, run_name='run1')
         gpt2.generate_to_file(self.session, filepath='generated.txt', length=self.num_words, temperature=0.9, prefix=self.prompt)
 
