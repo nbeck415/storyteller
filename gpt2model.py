437a6d1b7869417e36dd6df8a103ed6528124ae8
nbeck415 <natalielunbeck@yahoo.com>
2020-06-06 12:05:11 -0700

    Delete gpt2model.py

--- gpt2model.py
+++
@@ -1,6 +0,0 @@
-#Natalie Lunbeck
-#This program creates a GPT2 Model and uses it to generate text
-
-import torch
-import numpy as np
-from transformers import GPT2Model, GPT2Config, GPT2Tokenizer, GPT2TokenizerFast
